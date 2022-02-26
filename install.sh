7a2453716f9e4d2f3708f54d80689e25f841d60d
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-02-26 15:08:49 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 qemu qemu-system curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
-curl -L -o lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.6/providers/qemu.box
+curl -L -o win8.1.zip https://az792536.vo.msecnd.net/vms/VMBuild_20180102/VirtualBox/IE11/IE11.Win81.VirtualBox.zip
+unzip win8.1.zip
 echo "Windows 8 x86 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
