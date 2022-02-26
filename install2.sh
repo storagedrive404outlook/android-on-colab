cbf9cc5c752f749aebe61457512ecd8c54662b47
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-02-26 15:49:16 +0600

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,6 +1,6 @@
 echo "Windows 8 x86 Lite On Google Colab"
 echo Your VNC IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by Storagedrive404outlook
