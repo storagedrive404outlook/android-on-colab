780449af0c6dfa647eae14d85c7edfd8d12b813a
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-02-26 18:20:39 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@ echo Please wait for installing...
 sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 qemu qemu-system curl -y > /dev/null 2>&1
-echo Downloading Android Disk...
-curl -L -o android.7z https://jztkft.dl.sourceforge.net/project/osboxes/v/vm/1-A-dr/9.0/R2/andro_9.0-r2-VM-64bit.7z
-7z e android.7z
+echo Downloading Windows Disk...
+curl -L -o lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.1/providers/win8.box
+echo "Windows 10 x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
+echo Script by Storagedrive404outlook
+sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
