29d91466d8a4caae9d29dfd4c4a07392ff969175
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-02-26 15:57:17 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,5 @@ sudo apt install qemu-system-x86 qemu qemu-system curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
 curl -L -o android.7z https://altushost-swe.dl.sourceforge.net/project/osboxes/v/vb/1-A-d/Lineage-OS-CynagenMod/14.1/R5/64bit.7z
 7z e android.7z
-echo Rename The file in 64 file Lineage CM 14.1-R5.vdi to android.vdi
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
