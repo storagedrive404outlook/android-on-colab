4dadfcbacc1a74915df5265fc31e42a47e758c5f
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-02-26 18:03:18 +0600

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -2,4 +2,4 @@ echo "Android x86 Lite On Google Colab"
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by Storagedrive404outlook
-sudo qemu-system-x86_64 -vnc :0 -hda /content/android.vdi  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda /content/android.vmdk  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
