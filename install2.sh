ba7651b3ab87dfbaf6d6b5da76b971e0afab49ae
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-02-26 15:55:12 +0600

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -2,4 +2,4 @@ echo "Windows 8 x86 Lite On Google Colab"
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by Storagedrive404outlook
-sudo qemu-system-x86_64 -vnc :0 -hda /content/64bit/android  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda /content/android.vdi  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
